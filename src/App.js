6bed67035729d3a29984fefeed612e25c3617bf4
Marina Elizarova <seagirl1110@gmail.com>
2024-09-20 22:05:57 +0300

    feat: add mui components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
 
 function App() {
-  return <div className="App"></div>;
+  const [open, setOpen] = useState(false);
+
+  const openDialog = () => setOpen(true);
+  const closeDialog = () => setOpen(false);
+
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My Material UI App</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ pt: 2 }}>
+        <Typography variant="h4" gutterBottom>
+          Добро пожаловать в наше приложение!
+        </Typography>
+        <Button onClick={openDialog} variant="contained">
+          Открыть диалоговое окно
+        </Button>
+      </Container>
+      <Dialog open={open} onClose={closeDialog}>
+        <DialogTitle>Использовать Material UI?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Это простое React приложение с использованием Material UI. Вы можете
+            настроить его по своему усмотрению.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button>Отмена</Button>
+          <Button>Согласен</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default App;
